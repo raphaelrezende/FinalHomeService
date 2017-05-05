1ce8660bf18b3fb89a6fc1d823e606f25fd17915
raphaelrezende <raphael.rezende@gmail.com>
2017-05-05 18:49:33 -0300

    parte de categoria adicionada, conexão com o web service fake criado

---
+++ app/src/main/java/ria/inf/ufg/br/homeservice/presenter/categorias/ListaCategoriasActivity.java
@@ -0,0 +1,16 @@
+package ria.inf.ufg.br.homeservice.presenter.categorias;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ria.inf.ufg.br.homeservice.BaseActivity;
+import ria.inf.ufg.br.homeservice.R;
+
+public class ListaCategoriasActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_categorias);
+    }
+}
---
+++ app/src/main/java/ria/inf/ufg/br/homeservice/web/WebConnection.java
@@ -0,0 +1,63 @@
+package ria.inf.ufg.br.homeservice.web;
+
+/**
+ * Created by raphael on 05/05/17.
+ */
+
+import org.greenrobot.eventbus.EventBus;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+
+public abstract class WebConnection {
+
+    private static final String BASE_URL = "http://private-4a46c-homeservice.apiary-mock.com    ";
+    public static final MediaType JSON
+            = MediaType.parse("application/json; charset=utf-8");
+    private String serviceName;
+
+    public WebConnection(String serviceName){
+        this.serviceName = serviceName;
+    }
+
+    public void call() {
+        OkHttpClient client = new OkHttpClient();
+
+        String url = getUrl();
+
+        RequestBody body = RequestBody.create(JSON, getRequestContent());
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                EventBus.getDefault().post(new Exception("Verifique sua conexão"));
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                handleResponse(response);
+            }
+        });
+    }
+
+    public String getUrl(){
+        return BASE_URL + serviceName;
+    }
+
+    abstract String getRequestContent();
+
+    abstract void handleResponse(Response response);
+
+}
